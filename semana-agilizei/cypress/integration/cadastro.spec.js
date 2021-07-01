a0370779b9708ee671851fef139876d352ba8511
Railane Passos <railanepassos@gmail.com>
2021-07-01 10:49:33 -0300

    update: implementation Chanche.js library

--- semana-agilizei/cypress/integration/cadastro.spec.js
+++ semana-agilizei/cypress/integration/cadastro.spec.js
@@ -1,21 +1,26 @@
 /// <reference types="cypress" />
 
+//  Importando para nosso projeto a biblioteca Chance para geração de dados aleatórios.
+//  Isso irá nos ajudar no cadastro dentro da plataforma
+let Chance = require('chance')
+let chance = new Chance()
+
 context('Cadastro', () => {
   it('Cadastro de usuario no site', () => {
     //  Aqui acessa a rota que direciona a pagina de registro, ou seja, baseUrl + route
     cy.visit('/Register.html')
 
     //  type -> escreve em um campo
-    cy.get('input[placeholder="First Name"]').type('Teste')
-    cy.get('input[ng-model*=Last]').type('teste')
-    cy.get('textarea[ng-model="Adress"]').type('Rua vital')
-    cy.get('input[ng-model="EmailAdress"]').type('teste@teste.com')
-    cy.get('input[ng-model="Phone"]').type('71988776767')
+    cy.get('input[placeholder="First Name"]').type(chance.first())
+    cy.get('input[ng-model*=Last]').type(chance.last())
+    cy.get('textarea[ng-model="Adress"]').type(chance.address())
+    cy.get('input[ng-model="EmailAdress"]').type(chance.email())
+    cy.get('input[ng-model="Phone"]').type(chance.phone({ formatted: false }))
 
     //  check -> radio's e checkboxes
-    //  Selection gender
     //  cy.get('input[value="Male"]').check();
     cy.get('input[value="FeMale"]').check()
+
     //  Selection hobbies
     cy.get('input[type=checkbox]').check('Cricket')
     //  cy.get('input[type=checkbox]').check('Movies')
@@ -31,9 +36,35 @@ context('Cadastro', () => {
     cy.get('select#yearbox').select('1933')
     cy.get('select[ng-model="monthbox"]').select('June')
     cy.get('select#daybox').select('13')
-    cy.get('input#firstpassword').type('Javascript')
-    cy.get('input#secondpassword').type('Javascript')
+    cy.get('input#firstpassword').type('Javascript-99')
+    cy.get('input#secondpassword').type('Javascript-99')
     cy.get('button#submitbtn').click()
     //  cy.get('button#Button1').click()
+  })
+  it('Reset register form', () => {
+    cy.visit('/Register.html')
+    cy.get('input[placeholder="First Name"]').type(chance.first())
+    cy.get('input[ng-model*=Last]').type(chance.last())
+    cy.get('textarea[ng-model="Adress"]').type(chance.address())
+    cy.get('input[ng-model="EmailAdress"]').type(chance.email())
+    cy.get('input[ng-model="Phone"]').type(chance.phone({ formatted: false }))
+    cy.get('input[value="Male"]').check()
+    //cy.get('input[value="FeMale"]').check()
+    cy.get('input[type=checkbox]').check('Movies')
+    cy.get('input[type=checkbox]').check('Cricket')
+    //cy.get('input[type=checkbox]').check('Hockey')
+    cy.get('select#Skills').select('Javascript')
+    cy.get('select#countries').select('Australia')
+    cy.get('select#country').select('Bangladesh', { force: true })
+    cy.get('select#yearbox').select('1933')
+    cy.get('select[ng-model="monthbox"]').select('June')
+    cy.get('select#daybox').select('13')
+    cy.get('input#firstpassword').type('Javascript-99')
+    cy.get('input#secondpassword').type('Javascript-99')
+
+    //  Button reset
+    cy.get('button#Button1').click()
+
+    // Reset validate
   })
 })

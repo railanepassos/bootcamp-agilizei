0cdf34bcaa7bea3f572364ec940f6f38ee94dcc2
Railane Passos <railanepassos@gmail.com>
2021-07-05 22:50:30 -0300

    update: implementation cucumber HTML report

---
+++ semana-agilizei/cypress/reports/reporter.js
@@ -0,0 +1,20 @@
+var reporter = require('cucumber-html-reporter')
+
+var options = {
+  theme: 'bootstrap',
+  jsonDir: 'cypress/reports/cucumber-json',
+  output: 'cypress/reports/cucumber_report.html',
+  reportSuiteAsScenarios: true,
+  scenarioTimestamp: true,
+  launchReport: true,
+  metadata: {
+    'App Version': '0.3.2',
+    'Test Environment': 'STAGING',
+    Browser: 'Electron',
+    Platform: 'Big Sur',
+    Parallel: 'Scenarios',
+    Executed: 'Local'
+  }
+}
+
+reporter.generate(options)
